47ca19c8a47afc339b453025e75d8cf5db3f37a9
Devon Gasselyno <d.gasselyno@citis.kompasgramedia.com>
2025-08-12 15:23:44 +0700

    fix layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -60,7 +60,7 @@ const Index = () => {
 
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <div className="mx-auto flex min-h-screen max-w-screen-2xl">
+      <div className="mx-auto flex min-h-screen">
         {/* Sidebar */}
         <aside 
           className={`shrink-0 border-r border-sidebar-border bg-sidebar transition-all duration-300 ease-in-out ${
